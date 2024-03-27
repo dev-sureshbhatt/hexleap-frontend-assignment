8eebec66fb933e292611d7a71179c8ae1475067b
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-03-27 14:28:33 +0530

    conditionally rendered different div content based on card variant

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -6,22 +6,27 @@ interface CardProps {
 
 interface CardData {
   team_image?: string;
-  team_name?: string; 
-  total_events?: number; 
-  sport?: string; 
+  team_name?: string;
+  total_events?: number;
+  sport?: string;
   date_time?: string;
-  venue?: string; 
-  CTA_text?: string; 
-  ad_image?: string; 
-  ad_title?: string; 
+  venue?: string;
+  CTA_text?: string;
+  ad_image?: string;
+  ad_title?: string;
   ad_description?: string;
 }
 
-export default function Card({...props}: CardProps) {
-    const {variant, data} = props
+export default function Card({ ...props }: CardProps) {
+  const { variant, data } = props;
 
-    // defining CSS classes based on Card variant
-    const cardClass = `card${variant}`
-    
-    return <div className={cardClass}>{variant}</div>;
+  // defining CSS classes based on Card variant
+  const cardClass = `card${variant}`;
+  return(
+    <div className={cardClass}>
+      {variant === "event" && <>Hi, I am an Event Div</>}
+      {variant === "advertisement" && <>Hi, I am an Advertisement Div</>}
+      {variant === "collection" && <>Hi, I am a Collection Div</>}
+    </div>
+  )
 }
