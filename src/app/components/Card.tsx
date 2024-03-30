07ab4b0721de82f9562de5e5d7729cc2bc0ff2c1
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-03-30 11:05:28 +0530

    Tried making light/dark mode theme properties and conditionally render classes based on the isThemeLight (boolean) variable (initial setup, dark/light theme feature yet to complete )

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -20,6 +20,7 @@ interface CardData {
 
 export default function Card({ ...props }: CardProps) {
   const { variant, data } = props;
+  const isThemeLight = true 
 
   // defining CSS classes based on Card variant
   const cardClass = `card${variant}`;
@@ -28,7 +29,10 @@ export default function Card({ ...props }: CardProps) {
       {/* card variant type one styling/structure goes here */}
       {variant === "event" && (
         <>
-          <div className="bg-[#3B3E47] p-2 w-[237px] h-[511px]">
+        {/* below class is affected with dark/light mode toggle  */}
+        {/* <div className="bg-[#3B3E47] p-2 w-[237px] h-[511px]"> */}
+          {/* <div className={isThemeLight ? ("text-black bg-[#fff] p-2 w-[237px] h-[511px] ") : ("bg-[#3B3E47] p-2 w-[237px] h-[511px]")}> */}
+          <div className={`p-2 w-[237px] h-[511px] ${isThemeLight ? ("bg-[#FFFFFF] text-black"):  ("bg-[#3B3E47]")}`}>
             <div className={`image-div-${variant} w-[217px] h-[385px]`}>
               <img
                 className="object-fill w-[217px] h-[385px]"
@@ -38,7 +42,11 @@ export default function Card({ ...props }: CardProps) {
             <div className={`title-div-${variant} mt-3 mb-3`}>
               Sacramento River Cats
             </div>
-            <div className="flex justify-between p-3 bg-[#292B32]">
+            {/* below class is affected with dark/light mode toggle  */}
+            {/* <div className={isThemeLight ? ("flex justify-between p-3 bg-[#F7F7F8]") : ("flex justify-between p-3 bg-[#292B32]")}> */}
+            <div className={`flex justify-between p-3 ${isThemeLight ? ("bg-[#F7F7F8]") : ("bg-[#292B32]")}`}>
+
+            {/* <div className="flex justify-between p-3 bg-[#292B32]"> */}
               <div className="flex flex-col">
                 <span className="text-[12px] font-normal">Total Events</span>
                 <span className="text-[14px] font-medium">48 Events</span>
@@ -53,7 +61,9 @@ export default function Card({ ...props }: CardProps) {
       )}
 
       {variant === "advertisement" && (
+        
         <div className="bg-[#3B3E47] p-2 w-[237px] h-[511px] flex flex-col gap-3">
+        {/* above class is affected with dark/light mode toggle  */}
           <img
             className="object-fill w-[217px] h-[218px]"
             src="https://s3-alpha-sig.figma.com/img/c724/731e/f71497e0e3af85c77fa47046bb1f23cb?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YfHZuev7WUagkCJeDUVdHbkG5bqC2YmjXKJSZLKbyymg8PLUCF1d-3ITeG2OSpcKgHjVq3lbGf7eLgT0-RWii9vAbSSrltvD7RI88jX3A1p0QZTvL~643uJOzylJw00w1DWQKzrTEHLkK2D4jxJ4n3C9D03B-Odrn9zVjUWBi9mqnU5op991kONICO-AT7eCWb2APcuZxUGYnj8xpoj5L5modFS2ABEy2X1gaT0HXJZoVu2276JkODvrtjnC3F4~lM3f7rLQQam1iI8a3frGTCuFQfbXM3VAQ7nfBJDVDCcyeJcyc4m1oPldx2ONzo2CGn5jRfonRO5RlD-OHbierA__"
@@ -71,7 +81,12 @@ export default function Card({ ...props }: CardProps) {
       )}
 
       {variant === "collection" && (
+
+  // <div className="bg-[#3B3E47] p-2 w-[257px] h-[624px]">
+
+
         <div className="bg-[#3B3E47] p-2 w-[257px] h-[624px]">
+          {/* anove class is affected with dark/light mode toggle  */}
           <img
             className="object-fill w-[226px] h-[401px]"
             src="https://s3-alpha-sig.figma.com/img/bc7b/d2d1/a2c7e5def5b347470305864869a894b6?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YxtMQ-umkbEHlxMyMnstAs6rYbIBMoz6Y6e6X~XvOsr-rAQ2g4qOWln3w~caMGqtbtAtbCjyj-yjVTMTm1mZVFfJBk5KOXa491~GuF-sCId9i622FHtykZ-nY9kpx54GqgSCB2nGFH3HeaHmDDrU0aEinZpFFnqbmnXRqB9agt6U5PFCW70dobn1FS2-uCCVjf-JcgJzpDyt6xoO-4xWk6IWNKkJCo1K7Ad4KoEpKKDiFYyDhsXOKGZrevPj~1tdFMRrtdkTS8ckyL82IMwnzlMMtKVgab1EpeEiBiF-BW88PwJ0towH4BaKyuBx~E-cLgN0lW8hacJNXlAv-u1nFA__"
